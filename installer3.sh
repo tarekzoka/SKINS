12baf08a35c77a7b024b7d8858656194bbe0ce36
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-06-13 02:46:12 +0200

    Add files via upload

---
+++ installer3.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tarekzoka/NitroAdvanceFHD/main/installer3.sh -O - | /bin/sh
+###########################################
+###########################################
+#!/bin/sh
+echo
+opkg install --force-overwrite  https://github.com/tarekzoka/NitroAdvanceFHD/blob/main/SKIN-QATAR-2022-VXP-FHD-By-Muaath.ipk?raw=true
+wait
+exit#########################################################
+#########################################################
+MY_MAIN_URL="https://raw.githubusercontent.com/tarekzoka/"
+MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
+MY_TMP_FILE="/tmp/"$MY_FILE
+
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+if [ -f $MY_TMP_FILE ]; then
+
+	echo ''
+	echo $MY_SEP
+	echo 'Extracting ...'
+	echo $MY_SEP
+	echo ''
+	tar -xf $MY_TMP_FILE -C /
+	MY_RESULT=$?
+
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+	echo ''
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+        echo "#########################################################"
+        echo "#  NitroAdvanceFHD Skin $version INSTALLED SUCCESSFULLY      #"
+        echo "#                BY TAREK_TT - support on                   #"
+        echo "#   .................................................      #"
+        echo "#########################################################"
+        echo "#           your Device will RESTART Now                #"
+        echo "#########################################################"		
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
+else
+	echo ''
+	echo "Download failed !"
+	exit 1
+fi
+# ------------------------------------------------------------------------------------------------------------
