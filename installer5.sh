6ab24cb3176098c11e0330e7d6a126107c903a48
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-06-17 12:53:39 +0200

    Add files via upload

---
+++ installer5.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tarekzoka/SKINS/main/installer5.sh -O - | /bin/sh
+###########################################
+###########################################
+#!/bin/sh
+echo
+opkg install --force-overwrite  "https://drive.google.com/uc?id=1tdwXqro7f00s-iOOqwS17y93OazbtfV4&export=download"
+wait
+exit#########################################################
+#########################################################
+MY_MAIN_URL="https://raw.githubusercontent.com/tarekzoka/"
+MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
+MY_TMP_FILE="/tmp/"$MY_FILE
+
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+if [ -f $MY_TMP_FILE ]; then
+
+	echo ''
+	echo $MY_SEP
+	echo 'Extracting ...'
+	echo $MY_SEP
+	echo ''
+	tar -xf $MY_TMP_FILE -C /
+	MY_RESULT=$?
+
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+	echo ''
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+        echo "#########################################################"
+        echo "#  OP-ARTKOALA Skin $version INSTALLED SUCCESSFULLY      #"
+        echo "#                BY TAREK_TT - support on                   #"
+        echo "#   .................................................      #"
+        echo "#########################################################"
+        echo "#           your Device will RESTART Now                #"
+        echo "#########################################################"		
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
+else
+	echo ''
+	echo "Download failed !"
+	exit 1
+fi
+# 
+
+SKINS------------------------------------------------------------------------------------------------------------
+
