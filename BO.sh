ab45c258a85cc3cdd3d9bf56e76ddf1166b49dc7
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-07-03 16:01:20 +0200

    Update BO.sh

--- BO.sh
+++ BO.sh
@@ -1,5 +1,5 @@
 #!/bin/sh # 
- # # Command: wget https://raw.githubusercontent.com/tarekzoka/SKINS/main/BO.sh -qO - | /bin/sh # # 
+ # # Command: wget https://raw.githubusercontent.com/tarekzoka/SKINS/main/BO.sh -qO - | /bin/sh ###################################################################################### 
  echo " 
 MY_URL="https://raw.githubusercontent.com/tarekzoka/SKINS/main"
 MY_SKIN1="https://github.com/tarekzoka/SKINS/blob/main/enigma2-plugin_1.1.1-skin.BO-HLALA.OpenBH.Xevnt1.FHD.tar.gz?raw=truez"
