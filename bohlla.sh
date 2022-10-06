fdc70e940b36b9276768dcd3e56611e9fc67f7ea
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-10-06 05:46:24 +0200

    Update bohlla.sh

--- bohlla.sh
+++ bohlla.sh
@@ -1,16 +1,13 @@
-echo
 #!/bin/bash
 ######################################################################################
 ## Command=wget https://raw.githubusercontent.com/tarekzoka/SKINS/main/bohlla.sh -O - | /bin/sh
 ##
-###########################################
-###########################################
 #!/bin/sh
 echo
 ###########################################
 ###########################################
 opkg install --force-overwrite  "https://drive.google.com/uc?id=16ZNOygZd3YlJ-7FX-d0aoj8QVysVEKgO&export=download"
 sleep 2;
-exit 0###########################################
+exit 0
 
 
