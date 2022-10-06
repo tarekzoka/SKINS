c03a7e2e8ee1fa7b74b33b1251c81d9e41d20832
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-10-06 06:34:20 +0200

    Add files via upload

--- bohlla.sh
+++ bohlla.sh
@@ -1,12 +1,10 @@
-
-
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tarekzoka/SKINS/main/installer.sh -O - | /bin/sh
+###########################################
+###########################################
 #!/bin/sh
-#
-
-wget -O /tmp/enigma2-skin-bohlala-new_tarek_all.ipk "https://drive.google.com/uc?id=1nBdE3xR4zXo27xsSOc30GfsIycTwuOBd&export=download"
+echo
+opkg install --force-overwrite  https://drive.google.com/uc?id=1nBdE3xR4zXo27xsSOc30GfsIycTwuOBd&export=download
 wait
-opkg install --force-overwrite /tmp/*.ipk
-wait
-rm -r /tmp/enigma2-skin-bohlala-new_tarek_all.ipk
-
-sleep 2;
+exit
