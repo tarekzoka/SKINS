627335d111e339fb25a7e6f6ec1bdb8771620f31
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-10-06 06:26:36 +0200

    Add files via upload

--- bohlla.sh
+++ bohlla.sh
@@ -1,29 +1,12 @@
 
+
 #!/bin/sh
 #
 
-wget -O /tmp/BoHLALA_FHD.tar.gz "https://drive.google.com/uc?id=1nBdE3xR4zXo27xsSOc30GfsIycTwuOBd&export=download"
+wget -O /tmp/enigma2-skin-bohlala-new_tarek_all.ipk "https://drive.google.com/uc?id=1nBdE3xR4zXo27xsSOc30GfsIycTwuOBd&export=download"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -r /tmp/enigma2-skin-bohlala-new_tarek_all.ipk
 
-echo ""
-# Download and install plugin
-cd /tmp
-set -e
-echo "===> Downloading And Installing  BoHLALA_FHD.tar Please Wait ......"
-echo
-tar -xzf BoHLALA_FHD.tar.gz -C /
-set +e
-rm -f BoHLALA_FHD.tar.gz
-
-echo ""
-sync
-echo "##############################################################"
-echo "#         docsat audio INSTALLED SUCCESSFULLY             #"
-echo "#             UPLOADED BY ELIESAT                #"
-echo "##############################################################"
-echo "**************************************************************"
-echo "##############################################################"
-echo "#              RESTART YOUR STB NOW                  #"
-echo "##############################################################"
-
-exit 0
-
+sleep 2;
