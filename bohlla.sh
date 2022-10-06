73d15cfb748939abc51d618f955dcd8cb83bc03a
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-10-06 07:12:48 +0200

    Update bohlla.sh

--- bohlla.sh
+++ bohlla.sh
@@ -3,7 +3,7 @@ echo
 ######################################################################################
 ## Command= wget https://raw.githubusercontent.com/tarekzoka/SKINS/main/bohlla.sh -O - | /bin/sh
 echo
-opkg install --force-overwrite  "https://onedrive.live.com/embed?cid=CFCA224FBA296C58&resid=CFCA224FBA296C58%21199&authkey=AMn2XC9wXuKqCwc"
+opkg install --force-overwrite  "https://onedrive.live.com/download?cid=CFCA224FBA296C58&resid=CFCA224FBA296C58%21199&authkey=AMn2XC9wXuKqCwc"
 echo
 wait
 exit
