ec9020f046cde11f857d62eac2105d0efaadf4c7
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-02-10 02:26:20 +0200

    Add files via upload

---
+++ skins-premium-fhd.sh
@@ -0,0 +1,13 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-skins-premium-fhd_2.7_all.ipk "https://raw.githubusercontent.com/tarekzoka/SKINS/main/enigma2-plugin-skins-premium-fhd_2.7_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-skins-premium-fhd_2.7_all.ipk
+wait
+sleep 2;
+exit 0
+
