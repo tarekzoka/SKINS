f92676eac8fd1ba3b8faee78a4c8245c3890dbb4
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-08 01:31:11 +0200

    Add files via upload

---
+++ skin-SHADOW.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+
+wget -O /tmp/GREY-SHADOW-HD1.tar.gz "https://raw.githubusercontent.com/tarekzoka/SKINS//main/Muaath/GREY-SHADOW-HD1.tar.gz"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+rm -r /tmp/GREY-SHADOW-HD1.tar.gz
+
+echo "         UPLOADED BY TARK_HANFY    "
+
+
+killall -9 enigma2
+
+sleep 2
+
