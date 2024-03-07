79ee51db67e1ca2016f3f360237701f22e8dccd1
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-08 01:16:21 +0200

    Add files via upload

---
+++ skins-malek.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+
+wget -O /tmp/skins-malek-fhd_2.0.tar.gz "https://raw.githubusercontent.com/tarekzoka/SKINS/main/MALEK-FHD/skins-malek-fhd_2.0.tar.gz"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+rm -r /tmp/skins-malek-fhd_2.0.tar.gz
+
+killall -9 enigma2
+
+sleep 2;
